511193d7c57e085eb475edc494ac940c4e9e774a
Carmelo Rodríguez Rosalina <carmelorodriguezrosalina@gmail.com>
2024-06-22 11:08:20 +0200

    0.1.6

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -14,14 +14,27 @@ function Contacto({seccionAnimacion}) {
             leaveFrom="opacity-100 transform translate-y-0"
             leaveTo="opacity-0 transform translate-y-full"
         >
-            <div id="Contacto" className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-200 to-indigo-100 text-white"
+          <div id="Contacto" className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-200 to-indigo-100 text-white"
             style={{
               backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.9)), url(${street3})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
-            }}>
-                <h2 className="text-4xl font-bold">Contacto</h2>
-            </div>
+          }}>
+            <div className="mx-auto border-4 border-white">
+              <div className="flex mx-auto ml-5 mr-5 mb-3 mt-5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                </svg>  
+                <p className="text-2xl text-white mt-1 ml-5">carmelorodriguezrosalina@gmail.com</p>
+              </div>
+              <div className="flex text-center mb-2 ml-5 mr-5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                </svg>
+                <p className="text-2xl text-white mt-2 ml-5 mb-5">+34 634 84 66 23</p>
+              </div>
+          </div>
+        </div>
         </Transition>
     </>
   )
--- src/components/Tecnologias.jsx
+++ src/components/Tecnologias.jsx
@@ -1,31 +1,115 @@
-import { Transition } from '@headlessui/react'
-import React from 'react'
+import React from 'react';
+import { Transition } from '@headlessui/react';
 import lamparas from '../image/lamparas.jpg';
+import javascript from '../image/javascript.png';
+import react from '../image/react.png';
+import css from '../image/css.png';
+import html from '../image/HTML.png';
+import Tailwind from '../image/Tailwind.png';
+import PHP from '../image/PHP.png';
+import java from '../image/java.png';
+import laravel from '../image/laravel.png';
+import mysql from '../image/mysql.png';
+import visualStudio from '../image/visualStudio.png';
+import docker from '../image/docker.png';
+import eclipse from '../image/eclipse.png';
+import git from '../image/git.svg';
+import figma from '../image/figma.png';
+import nodejs from '../image/nodejs.png';
 
-function Tecnologias({seccionAnimacion}) {
+function Tecnologias({ seccionAnimacion }) {
   return (
     <>
-        <Transition
-            show={seccionAnimacion === 'Tecnologias'}
-            enter="transition ease-out duration-500"
-            enterFrom="opacity-0 transform translate-x-full"
-            enterTo="opacity-100 transform translate-x-0"
-            leave="transition ease-in duration-300"
-            leaveFrom="opacity-100 transform translate-x-0"
-            leaveTo="opacity-0 transform translate-x-full"
+      <Transition
+        show={seccionAnimacion === 'Tecnologias'}
+        enter="transition ease-out duration-500"
+        enterFrom="opacity-0 transform translate-x-full"
+        enterTo="opacity-100 transform translate-x-0"
+        leave="transition ease-in duration-300"
+        leaveFrom="opacity-100 transform translate-x-0"
+        leaveTo="opacity-0 transform translate-x-full"
+      >
+        <div
+          id="Tecnologias"
+          className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-800 to-purple-800 text-white"
+          style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.9)), url(${lamparas})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
         >
-            <div id="Tecnologias" className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-800 to-purple-800 text-white"
-            style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.9)), url(${lamparas})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-              }}>
-                <h2 className="text-4xl font-bold">Tecnologías</h2>
-                <p className="mt-2">Scroll Down for next slide</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 max-w-5xl w-full px-8">
+            <div className="flex flex-col items-center p-6 mr-20">
+              <h3 className="text-2xl font-bold mb-4">FRONTEND</h3>
+              <div className="flex space-x-4">
+                <div className="w-24 h-24">
+                  <img src={html} alt="HTML Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                <div className="w-24 h-24">
+                  <img src={css} alt="CSS Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                <div className="w-24 h-24">
+                  <img src={javascript} alt="JavaScript Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                <div className="w-24 h-24">
+                  <img src={react} alt="React Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                
+                <div className="w-24 h-24">
+                  <img src={Tailwind} alt="Tailwind Logo" className="w-full h-full object-contain p-2" />
+                </div>
+              </div>
             </div>
-        </Transition>
+            <div className="flex flex-col items-center p-6 rounded-lg ml-20">
+              <h3 className="text-2xl font-bold mb-4">BACKEND</h3>
+              <div className="flex space-x-4">
+                <div className="w-24 h-24">
+                  <img src={laravel} alt="laravel Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                <div className="w-24 h-24">
+                  <img src={java} alt="java Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                <div className="w-24 h-24">
+                  <img src={mysql} alt="mysql Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                <div className="w-24 h-24">
+                  <img src={PHP} alt="PHP Logo" className="w-full h-full object-contain p-2" />
+                </div>  
+              </div>
+            </div>
+            <div className="flex flex-col items-center p-6 rounded-lg mr-20">
+              <h3 className="text-2xl font-bold mb-4">HERRAMIENTAS</h3>
+                <div className="flex space-x-4">
+                  <div className="w-24 h-24">
+                    <img src={git} alt="git Logo" className="w-full h-full object-contain p-2" />
+                  </div>
+                  <div className="w-24 h-24">
+                    <img src={visualStudio} alt="visualStudio Logo" className="w-full h-full object-contain p-2" />
+                  </div>
+                  <div className="w-24 h-24">
+                    <img src={eclipse} alt="eclipse Logo" className="w-full h-full object-contain p-2" />
+                  </div>
+                  <div className="w-24 h-24">
+                    <img src={docker} alt="docker Logo" className="w-full h-full object-contain p-2" />
+                  </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-center p-6 rounded-lg ml-20">
+              <h3 className="text-2xl font-bold mb-4">APRENDIENDO</h3>
+              <div className="flex space-x-4">
+                <div className="w-24 h-24">
+                  <img src={nodejs} alt="nodejs Logo" className="w-full h-full object-contain p-2" />
+                </div>
+                <div className="w-24 h-24">
+                  <img src={figma} alt="figma Logo" className="w-full h-full object-contain p-2" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Transition>
     </>
-  )
+  );
 }
 
-export default Tecnologias
+export default Tecnologias;
