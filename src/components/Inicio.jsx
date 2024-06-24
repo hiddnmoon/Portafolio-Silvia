94f86c4941706d094bba516bcf7818a28aa59366
Carmelo Rodríguez Rosalina <carmelorodriguezrosalina@gmail.com>
2024-06-24 18:41:31 +0200

    0.1.9

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -48,16 +48,20 @@ function Inicio({ seccionAnimacion, navegacion }) {
             backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.9)), url(${street})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-            minHeight: '100vh',
           }}
         >
-          <div className={`flex ${isMobile ? 'flex-col-reverse' : 'flex-row'} items-center md:flex-row`}>
-            <div className={`flex-grow ${isMobile ? 'w-full' : 'md:w-3/4'} flex flex-col justify-center p-4 ${isMobile ? 'mt-4' : 'md:mt-0'}`}>
-              <h2 className="text-4xl font-base text-white">
+          <div className={`flex ${isMobile ? 'flex-col' : 'flex-row'} items-center`}>
+            {isMobile && (
+              <div className="w-full flex items-center justify-center p-4 mb-8">
+                <img src={yo} className='rounded-full w-60 h-60 object-cover hover:scale-110' alt="Imagen de Carmelo Rodríguez" />
+              </div>
+            )}
+            <div className={`flex-grow ${isMobile ? 'w-full' : 'md:w-3/4'} flex flex-col justify-center p-4 ${isMobile ? 'mb-8' : 'md:mb-0'}`}>
+              <h2 className="text-4xl font-base text-white text-center md:text-left">
                 ¡Hola, soy <span className="font-extrabold text-white">Carmelo Rodríguez</span>!
               </h2>
-              <h2 className="text-3xl font-bold text-white mt-2">Frontend Developer</h2>
-              <p className="text-white mt-3 text-justify max-w-md">
+              <h2 className="text-3xl font-bold text-white mt-2 text-center md:text-left">Frontend Developer</h2>
+              <p className="text-white mt-3 text-justify max-w-3xl mx-auto md:mx-0">
                 Desde temprana edad, descubrí mi pasión por el arte y la creatividad. Esta inclinación artística me ha
                 acompañado a lo largo de los años y la he canalizado hacia el diseño web, fusionando estética y funcionalidad
                 en cada proyecto. Mi formación en Desarrollo de Aplicaciones Web, complementada con un fuerte enfoque en
@@ -65,35 +69,29 @@ function Inicio({ seccionAnimacion, navegacion }) {
                 y eficientes. Nacido en Huelva, España, mi objetivo es aprovechar mi creatividad innata para desarrollar soluciones
                 web innovadoras y de alta calidad.
               </p>
-              <div className="flex justify-center md:justify-start mt-4">
-                <a href="https://github.com/crodros2601" target="_blank" rel="noopener noreferrer">
-                  <img src={gitHub} className="w-12" alt="Icono de GitHub" />
+              <div className={`flex justify-center md:justify-start mt-4 ${isMobile ? 'flex-wrap space-x-3' : ''}`}>
+              <a href="https://github.com/crodros2601" target="_blank" rel="noopener noreferrer">
+                  <img src={gitHub} className='w-16' alt="Icono de GitHub" />
+                </a>
+                <a href="https://www.linkedin.com/in/carmelorodriguezrosalina/" target="_blank" rel="noopener noreferrer">
+                  <img src={linkedin} className='w-12 mt-3 mr-4' alt="Icono de Linkedin" />
+                </a>
+                <a href="https://www.instagram.com/karmelartem" target="_blank" rel="noopener noreferrer">
+                  <img src={instagram} className='w-9 mt-7 mr-5' alt="Icono de Instagram" />
                 </a>
                 <div>
-                  <a href="https://www.linkedin.com/in/carmelorodriguezrosalina/" target="_blank" rel="noopener noreferrer">
-                    <img src={linkedin} className="w-10 ml-4" alt="Icono de Linkedin" />
-                  </a>
+                  <button className='w-24 p-1 mt-8 bg-white rounded-full font-extrabold' onClick={descargarCV}>CV</button>
                 </div>
                 <div>
-                  <a href="https://www.instagram.com/karmelartem" target="_blank" rel="noopener noreferrer">
-                    <img src={instagram} className="w-8 ml-4" alt="Icono de Instagram" />
-                  </a>
-                </div>
-                <div>
-                  <button className="w-20 p-1 mt-4 bg-white rounded-full font-extrabold" onClick={descargarCV}>
-                    CV
-                  </button>
-                </div>
-                <div>
-                  <button className="w-28 font-extrabold p-1 mt-4 ml-4 bg-white rounded-full" onClick={() => navegacion("Contacto")}>
-                    CONTACTO
-                  </button>
+                  <button onClick={()=>navegacion("Contacto")} className='w-32 font-extrabold p-1 mt-8 ml-5 bg-white rounded-full'>CONTACTO</button>
                 </div>
               </div>
             </div>
-            <div className={`flex-none ${isMobile ? 'w-full mt-4' : 'md:w-1/4'} flex items-center justify-center p-4`}>
-              <img src={yo} className="rounded-full w-48 h-48 object-cover hover:scale-110" alt="Imagen de Carmelo Rodríguez" />
-            </div>
+            {!isMobile && (
+              <div className={`flex-none ${isMobile ? 'w-full mt-8' : 'md:w-1/4'} flex items-center justify-center p-4`}>
+                <img src={yo} className='rounded-full w-60 h-60 object-cover hover:scale-110' alt="Imagen de Carmelo Rodríguez" />
+              </div>
+            )}
           </div>
         </div>
       </Transition>
