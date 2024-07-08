e144fb3118f0556576932882df4d1d22d29b2d56
Carmelo Rodríguez Rosalina <carmelorodriguezrosalina@gmail.com>
2024-07-08 17:08:53 +0200

    1.0

--- src/components/ProyectoDetalle1.jsx
+++ src/components/ProyectoDetalle1.jsx
@@ -62,7 +62,7 @@ function ProyectoDetalle1({ seccionAnimacion }) {
               <iframe title="video" width="100%" height="315" src="https://www.youtube.com/embed/_5vwORc34s8" frameborder="0" allowfullscreen></iframe>
             </div>
             <p className="text-lg text-gray-200">
-            Introducción del Proyecto El cortometraje presentado para el certamen de cortometrajes de Castilblanco de los Arroyos, 
+            <span className='font-bold'>Introducción del Proyecto</span> El cortometraje presentado para el certamen de cortometrajes de Castilblanco de los Arroyos, 
             llamado Símbolo - 5mm. Se narra la historia de un joven que vive bajo el control estricto de sus padres y su eventual rebelión 
             contra ellos. Esta narrativa se desarrolla tanto en entorno cerrado como fuera, donde se capturan los momentos de tensión y liberación 
             del protagonista y se ve la contraposición de sus emociones.
@@ -71,36 +71,42 @@ function ProyectoDetalle1({ seccionAnimacion }) {
             <br></br>
 
             <p className="text-lg text-gray-200">
-            Equipo de Producción: El equipo estuvo compuesto por 10 personas, permitiendo una distribución de roles más específica y colaborativa. 
+            <span className='font-bold'>Equipo de Producción:</span> El equipo estuvo compuesto por 10 personas, permitiendo una distribución de roles más específica y colaborativa. 
             En esta ocasión, mis responsabilidades se centraron en las siguientes áreas:
             </p>
 
             <br></br>
 
             <ul className="list-disc list-inside text-gray-200">
-              <li className='text-lg'>Maquillaje y Vestuario: Diseñé y supervisé el maquillaje y vestuario de los personajes, asegurando que cada 
+              <li className='text-lg'><span className='font-bold'>Maquillaje y Vestuario:</span> Diseñé y supervisé el maquillaje y vestuario de los personajes, asegurando que cada 
                 detalle contribuyera a la construcción de sus personalidades y a la atmósfera del cortometraje.
               </li>
-              <li className='text-lg'>Script: Me encargué de la continuidad y de asegurar que todos los elementos visuales y narrativos se 
+              <li className='text-lg'><span className='font-bold'>Script:</span> Me encargué de la continuidad y de asegurar que todos los elementos visuales y narrativos se 
                 mantuvieran coherentes a lo largo de las diferentes tomas y escenas.
               </li>
             </ul>
 
             <p className="text-lg text-gray-200">
-            Elementos Destacables:
+            <span className='font-bold'>Elementos Destacables:</span>
             </p>
 
             <ul className="list-disc list-inside text-gray-200">
-              <li className='text-lg'>Narrativa: La historia resuena con cualquier audiencia que haya experimentado conflictos 
+              <li className='text-lg'><span className='font-bold'>Narrativa:</span> La historia resuena con cualquier audiencia que haya experimentado conflictos 
                 familiares y la lucha por la independencia personal.
               </li>
-              <li className='text-lg'>Dirección de Fotografía: Los planos nocturnos fueron diseñados para ser particularmente llamativos, 
+              <li className='text-lg'><span className='font-bold'>Dirección de Fotografía:</span> Los planos nocturnos fueron diseñados para ser particularmente llamativos, 
                 utilizando técnicas de iluminación y encuadre que destacan la tensión y la emoción del protagonista.
               </li>
-              <li className='text-lg'>Producción Local: La elección de grabar en una casa local no solo aporta autenticidad al cortometraje,
+              <li className='text-lg'><span className='font-bold'>Producción Local:</span> La elección de grabar en una casa local no solo aporta autenticidad al cortometraje,
                 sino que también subraya la conexión del proyecto con el pueblo de Castilblanco de los Arroyos.
               </li>
             </ul>
+            
+            <p className="text-lg text-gray-200">
+            <span className='font-bold'><span className='font-bold'>Conclusión:</span> Este cortometraje no solo se enfoca en una narrativa poderosa y relevante, 
+              sino que también muestra un alto nivel de colaboración y creatividad del equipo. Cada miembro contribuyó de manera 
+              significativa para lograr un producto final que cumple con los estándares del certamen y refleja la dedicación y talento del grupo.
+            </p>
 
           </div>
         </div>
