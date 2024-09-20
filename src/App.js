3fc41faf6c75fa6ab9d97af8fede57d3f79faa5a
Carmelo Rodríguez Rosalina <carmelorodriguezrosalina@gmail.com>
2024-09-20 08:10:21 +0200

    analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Slider from './components/Slider';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
  
   return (
     <div>
       <Slider />
+      <Analytics/>
     </div>
   );
 }
